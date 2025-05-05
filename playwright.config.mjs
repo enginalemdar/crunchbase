5076583b2432c4d6c94941af58ba428cb90c2f07
enginalemdar <162575473+enginalemdar@users.noreply.github.com>
2025-05-05 20:04:50 +0300

    Update and rename playwright.config.js to playwright.config.mjs

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'playwright-extra';
 import stealth from 'playwright-extra-plugin-stealth';
 
 export default defineConfig({
-  // Stealth plugin'ini ekle
-  plugins: [stealth()],
+  plugins: [stealth()]
 });
